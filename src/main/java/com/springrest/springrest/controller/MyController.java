d7e150c5340377961a4c92bf06b83fe18eb40bd6
amitthakur2013 <amitthakur2013@gmail.com>
2021-03-13 23:41:42 +0530

    1st

---
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -0,0 +1,37 @@
+package com.springrest.springrest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.entities.Course;
+import com.springrest.springrest.services.CourseService;
+
+import java.util.List;
+
+@RestController
+public class MyController {
+	
+	@Autowired
+	private CourseService courseService;
+	
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to courses application!";
+	}
+	
+	// get the courses
+	
+	@GetMapping("/courses")
+	public List<Course> getCourses(){
+		return this.courseService.getCourses();
+	}
+	
+	@GetMapping("/courses/{courseId}")
+	public Course getCourse(@PathVariable String courseId) {
+		return this.courseService.getCourse(Long.parseLong(courseId));
+	}
+}
---
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,33 @@
+package com.springrest.springrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.entities.Course;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	List<Course> list;
+	public CourseServiceImpl() {
+		 list=new ArrayList<>();
+		 list.add(new Course(145,"Java Core","This is detailed desc of jva!"));
+		 list.add(new Course(443,"Spring boot","This is desc of spring boot"));
+	}
+	
+	@Override
+	public List<Course> getCourses() {
+		return list;
+	}
+
+	@Override
+	public Course getCourse(long courseId) {
+		for(Course course:list) {
+			if (course.getId() == courseId) return course;
+		}
+		return null;
+	}
+
+}
